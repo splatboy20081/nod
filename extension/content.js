c8982b419565bbb41f84867a96c46fbebc9bd6bb
Jamie Carr <jamie.carr.07@gmail.com>
2020-03-20 19:06:26 +0000

    added emoji imgs instead of text

--- extension/content.js
+++ extension/content.js
@@ -1,84 +1,129 @@
 chrome.runtime.onMessage.addListener(request => {
   if (request.message === "init_action") {
+    //
+    // Sudo Imports?
+    //
+
+    const createTray = () => {
+      const thumbsup = createBtn("img/thumb.png");
+      const wave = createBtn("img/wave.gif", true);
+      const heart = createBtn("img/love.gif", true);
+      const laugh = createBtn("img/laugh.gif", true);
+      const clap = createBtn("img/clap.gif", true);
+
+      var wrapper = document.createElement("DIV");
+      wrapper.classList.add("nod-appWrapper");
+      wrapper.append(thumbsup);
+
+      var tray = document.createElement("DIV");
+      tray.classList.add("nod-tray");
+      tray.append(heart);
+      tray.append(laugh);
+      tray.append(wave);
+      tray.append(clap);
+
+      wrapper.addEventListener("mouseenter", () => {
+        wrapper.appendChild(tray);
+      });
+
+      wrapper.addEventListener("mouseleave", () => {
+        wrapper.removeChild(tray);
+      });
+
+      return wrapper;
+    };
+
+    const createMessageWrapper = () => {
+      var wrapper = document.createElement("DIV");
+      wrapper.style = "position: fixed; bottom:100px; left:20px; z-index: 10000000";
+      return wrapper;
+    };
+
+    const createBtn = (emojiPath, small) => {
+      var btn = document.createElement("DIV");
+      btn.classList.add("nod-btn");
+      small && btn.classList.add("small");
+
+      var img = document.createElement("IMG");
+      const emoji = chrome.runtime.getURL(emojiPath);
+      img.setAttribute("src", emoji);
+      img.classList.add("nod-emoji");
+
+      btn.appendChild(img);
+      btn.addEventListener(
+        "click",
+        debounce(() => {
+          wsClient.send(JSON.stringify({ id: request.id, emoji: emoji, message: username, img: avatar.src }));
+        }, 400)
+      );
+      return btn;
+    };
+
+    const createMessage = message => {
+      const messageData = JSON.parse(message);
+
+      var messageWrapper = document.createElement("DIV");
+      messageWrapper.classList.add("nod-message");
+
+      var img = document.createElement("IMG");
+      img.setAttribute("src", messageData.img);
+      img.classList.add("nod-avatar");
+
+      var emojiWrapper = document.createElement("DIV");
+      emojiWrapper.classList.add("nod-emoji-wrapper");
+
+      var emoji = document.createElement("IMG");
+      emoji.setAttribute("src", messageData.emoji);
+      emoji.classList.add("nod-emoji");
+
+      emojiWrapper.appendChild(emoji);
+      messageWrapper.appendChild(emojiWrapper);
+      messageWrapper.appendChild(img);
+      messageWrapper.innerHTML += `${messageData.message}`;
+
+      return messageWrapper;
+    };
+
+    function getElementByXpath(path) {
+      return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    }
+
+    const debounce = (func, delay) => {
+      let inDebounce;
+      return function() {
+        const context = this;
+        const args = arguments;
+        clearTimeout(inDebounce);
+        inDebounce = setTimeout(() => func.apply(context, args), delay);
+      };
+    };
+
+    // Variables
+
     const wsClient = new WebSocket("wss://ma711m87kd.execute-api.us-east-1.amazonaws.com/dev/");
-    const appWrapper = createWrapper();
-    const messageWrapper = createMessageWrapper();
 
     const avatar = getElementByXpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[3]/div[3]/div/div[1]/div[2]/div/img');
     const name = getElementByXpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[3]/div[3]/div/div[1]/div[2]/div/div/div');
     const username = name["title"].split(" ")[0];
 
-    document.body.appendChild(appWrapper);
-    document.body.appendChild(messageWrapper);
+    const appWrapper = createTray();
+    const messageWrapper = createMessageWrapper();
+    // Main
 
     wsClient.addEventListener("open", () => {
-      const button = createBtn(wsClient, request.id, username, avatar.src);
-      appWrapper.append(button);
+      document.body.appendChild(appWrapper);
+      document.body.appendChild(messageWrapper);
       wsClient.send(JSON.stringify({ route: "join", data: { id: request.id } }));
     });
 
     wsClient.addEventListener("message", event => {
       messageWrapper.insertAdjacentElement("afterbegin", createMessage(event.data));
       setTimeout(() => {
-        messageWrapper.firstChild.style = "opacity: 0";
+        messageWrapper.lastChild.style = "opacity: 0";
         setTimeout(() => {
-          messageWrapper.removeChild(messageWrapper.firstChild);
+          messageWrapper.removeChild(messageWrapper.lastChild);
         }, 300);
-      }, 3000);
+      }, 5000);
     });
   }
 });
-
-const createWrapper = () => {
-  var wrapper = document.createElement("DIV");
-  wrapper.style = "position: fixed; top:20px; left:20px; z-index: 10000000";
-  return wrapper;
-};
-
-const createMessageWrapper = () => {
-  var wrapper = document.createElement("DIV");
-  wrapper.style = "position: fixed; bottom:100px; left:20px; z-index: 10000000";
-  return wrapper;
-};
-
-const createBtn = (wsClient, id, name, img) => {
-  var btn = document.createElement("DIV");
-  btn.classList.add("handsup-btn");
-  btn.innerHTML = "👍️";
-  btn.addEventListener(
-    "click",
-    debounce(args => {
-      wsClient.send(JSON.stringify({ id: id, message: name, img: img }));
-    }, 300)
-  );
-  return btn;
-};
-
-const createMessage = message => {
-  const messageData = JSON.parse(message);
-
-  var messageWrapper = document.createElement("DIV");
-  messageWrapper.classList.add("handsup-message");
-
-  var img = document.createElement("IMG");
-  img.setAttribute("src", messageData.img);
-  img.classList.add("handsup-avatar");
-
-  messageWrapper.appendChild(img);
-  messageWrapper.innerHTML += `${messageData.message} 👍️`;
-  return messageWrapper;
-};
-
-function getElementByXpath(path) {
-  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
-
-const debounce = (func, delay) => {
-  let inDebounce;
-  return function() {
-    const context = this;
-    const args = arguments;
-    clearTimeout(inDebounce);
-    inDebounce = setTimeout(() => func.apply(context, args), delay);
-  };
-};
