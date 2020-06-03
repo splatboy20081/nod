5d68d252d6dc2d01243b9f9b7d6042b8443cb405
splatboy20081 <64037464+splatboy20081@users.noreply.github.com>
2020-06-03 13:30:22 -0700

    Some capitalization

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onUpdateAvailable.addListener(function(details) {
-  console.log("updating to version " + details.version);
+  console.log("Updating to version " + details.version);
   chrome.runtime.reload();
 });
 
@@ -7,10 +7,10 @@ chrome.runtime.onMessageExternal.addListener(function(request, sender, sendRespo
   if (request.reload) {
     chrome.runtime.requestUpdateCheck(function(status) {
       if (status == "update_available") {
-        console.log("update pending...");
+        console.log("Update pending...");
         sendResponse({ updateAvailable: true, throttled: false });
       } else if (status == "no_update") {
-        console.log("no update found");
+        console.log("No update found");
         sendResponse({ updateAvailable: false, throttled: false });
       } else if (status == "throttled") {
         console.log("Oops, I'm asking too frequently - I need to back off.");
